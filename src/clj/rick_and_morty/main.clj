b9481b74e98293586a235a72447355b71c1d7f53
Mårten Larsson <mala235813@gmail.com>
2020-11-13 15:03:43 +0100

    Add index.html and impl echo

--- src/clj/rick_and_morty/main.clj
+++ src/clj/rick_and_morty/main.clj
@@ -1,5 +1,6 @@
 (ns rick-and-morty.main
-  (:require [io.pedestal.http :as http]))
+  (:require [clojure.pprint :as pp]
+            [io.pedestal.http :as http]))
 
 (defn hello
   [request]
@@ -9,7 +10,8 @@
 (defn echo
   [request]
   {:status 200
-   :body "Should echo request here"})
+   :headers {"Content-Type" "text/plain"}
+   :body (with-out-str (pp/pprint request))})
 
 (def routes #{["/" :get hello :route-name ::root]
               ["/echo" :any echo :route-name ::echo]})
@@ -23,7 +25,8 @@
                      ::http/host "0.0.0.0"
                      ::http/port 80
                      ::http/join? do-join
-                     ::http/routes routes}
+                     ::http/routes routes
+                     ::http/resource-path "/public"}
          server (http/create-server server-map)]
      (http/start server)
      (reset! instance server))))
