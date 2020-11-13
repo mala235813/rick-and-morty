6b5db0709231f319f00653e5eb5e7df2b0d257e7
Mårten Larsson <mala235813@gmail.com>
2020-11-13 14:22:59 +0100

    Web server

--- src/clj/rick_and_morty/main.clj
+++ src/clj/rick_and_morty/main.clj
@@ -1,5 +1,29 @@
-(ns rick-and-morty.main)
+(ns rick-and-morty.main
+  (:require [io.pedestal.http :as http]))
+
+(defn hello
+  [request]
+  {:status 200
+   :body "Rick and morty app!"})
+
+(defn echo
+  [request]
+  {:status 200
+   :body "Should echo request here"})
+
+(def routes #{["/" :get hello :route-name ::root]
+              ["/echo" :any echo :route-name ::echo]})
+
+(defn start
+  []
+  (let [server-map {::http/type :jetty
+                    ::http/host "0.0.0.0"
+                    ::http/port 80
+                    ::http/join? true
+                    ::http/routes routes}
+        server (http/create-server server-map)]
+    (http/start server)))
 
 (defn -main
   [& args]
-  (println "Rick and morty app!"))
+  (start))
