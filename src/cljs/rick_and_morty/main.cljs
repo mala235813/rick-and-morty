2741aa811829ea8bfc0fa68f54cca00a0cd7b12a
Mårten Larsson <mala235813@gmail.com>
2020-11-20 14:33:30 +0100

    Setup dev hot reloading

--- src/cljs/rick_and_morty/main.cljs
+++ src/cljs/rick_and_morty/main.cljs
@@ -1,5 +1,16 @@
-(ns rick-and-morty.main)
+(ns rick-and-morty.main
+  (:require [reagent.dom :as rdom]))
+
+(defn start
+  []
+  (rdom/render [:div
+                [:h1 "Rick and morty!!!"]
+                [:p "Hot reload!!!"]] (js/document.getElementById "app")))
+
+(defn ^:dev/after-load restart
+  []
+  (start))
 
 (defn init!
   []
-  (println "Rick and morty!!!"))
+  (start))
