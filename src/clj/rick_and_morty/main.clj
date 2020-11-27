55f8ae99c92f6cde907be66bfebaa34dd06597d0
Mårten Larsson <mala235813@gmail.com>
2020-11-27 13:59:58 +0100

    Add crux

--- src/clj/rick_and_morty/main.clj
+++ src/clj/rick_and_morty/main.clj
@@ -1,5 +1,6 @@
 (ns rick-and-morty.main
   (:require [clojure.pprint :as pp]
+            [crux.api :as crux]
             [io.pedestal.http :as http]))
 
 (defn hello
@@ -20,9 +21,22 @@
 
 (defonce instance (atom nil))
 
+(defonce crux-node (atom nil))
+
+(defn start-crux
+ []
+ (when (nil? @crux-node)
+   (reset! crux-node (crux/start-node {}))))
+
+(defn stop-crux
+ []
+ (when (some? @crux-node)
+   (.close @crux-node)))
+
 (defn start
   ([] (start false))
   ([do-join]
+   (start-crux)
    (let [server-map {::http/type :jetty
                      ::http/host "0.0.0.0"
                      ::http/port 80
@@ -37,7 +51,8 @@
   []
   (when (some? (deref instance))
     (http/stop (deref instance))
-    (reset! instance nil)))
+    (reset! instance nil))
+  (stop-crux))
 
 (defn restart
   ([] (restart false))
@@ -53,6 +68,7 @@
   (restart)
 
   )
+
 (defn -main
   [& args]
   (start true))
